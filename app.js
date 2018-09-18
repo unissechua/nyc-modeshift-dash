320d1d8e8662f76f0cae4641ab2dbb3a0a9368a8
Unisse Chua <unissechua@users.noreply.github.com>
2018-09-18 15:43:32 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+import {MDCRipple} from '@material/ripple';
+import {MDCSelect} from '@material/select';
+
+// const ripple = new MDCRipple(document.querySelector('.foo-button'));
+
+
+const select = new MDCSelect(document.querySelector('.mdc-select'));
+// select.listen('change', () => {
+//   alert(`Selected option at index ${select.selectedIndex} with value "${select.value}"`);
+// });
