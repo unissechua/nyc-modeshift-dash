79a7c06be11eddf1f621a90ad0a0b1af808e6b68
Unisse Chua <unissechua@users.noreply.github.com>
2018-09-19 09:28:16 +0800

    add map

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ import {MDCSelect} from '@material/select';
 // const ripple = new MDCRipple(document.querySelector('.foo-button'));
 
 
-const select = new MDCSelect(document.querySelector('.mdc-select'));
+// const select = new MDCSelect(document.querySelector('.mdc-select'));
 // select.listen('change', () => {
 //   alert(`Selected option at index ${select.selectedIndex} with value "${select.value}"`);
 // });
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidW5pc3NlY2h1YSIsImEiOiJjamR3NG11dHMzYThnMzNxcDZmYWhsdXBxIn0.0XHPtsEvZJjMI3Ev2GElKA';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v9',
+    center: [-73.98, 40.75],
+    zoom: 11
+});
